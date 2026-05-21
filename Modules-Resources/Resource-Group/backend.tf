69fc0a1b844c910a340c18f1f7026c0c0a922add
nsdobal <nsdobal@gmail.com>
2026-05-21 19:51:11 +0530

    refresh resource-group module completely with pipeline

--- Modules-Resources/Resource-Group/backend.tf
+++ Modules-Resources/Resource-Group/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "nd-rg-tfstate"
     storage_account_name = "ndstorageaccounttfstate1"
     container_name       = "nd-tfstate"
-    key                  = "test.tfstate"
+    key                  = "pipeline.tfstate"
   }
 }
